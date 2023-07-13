e489cbe97e73b2aafc1c7514365a7b978d367314
Greeeeeezly <zolotarev.grisha.2012@gmail.com>
2023-07-13 11:29:42 +0300

    Task 1.3 add single table

--- src/main/java/org/example/models/Vehicle.java
+++ src/main/java/org/example/models/Vehicle.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 @Table(name = "vehicles")
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Entity
 public abstract class Vehicle {
     @Id
