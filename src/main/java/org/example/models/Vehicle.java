9db17383cf55f630a7e7e3be9fcb24910d7877af
Greeeeeezly <zolotarev.grisha.2012@gmail.com>
2023-07-15 19:40:17 +0300

    Task 1.1 Change Car.seats type

--- src/main/java/org/example/models/Vehicle.java
+++ src/main/java/org/example/models/Vehicle.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 @Table(name = "vehicles")
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Entity
 public abstract class Vehicle {
     @Id
